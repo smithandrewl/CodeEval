73c83fd2d313e8f5fdd5da7726292a31e8d51649
Andrew Smith <smith.andrew.l@gmail.com>
2014-01-19 16:54:17 -0600

    Added solution for the CodeEval "Lower Case" problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/lowercase/Main.scala
@@ -0,0 +1,9 @@
+package com.smithandrewl.codeeval.lowercase
+
+object Main extends App {
+  val source      = scala.io.Source.fromFile(args(0))
+  val lines       = source.getLines().filter(_.length > 0)
+  val transformed = lines.map(_.toLowerCase)
+
+  print(transformed.mkString("\n"))
+}
