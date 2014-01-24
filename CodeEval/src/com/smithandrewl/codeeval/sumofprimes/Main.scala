592ae3eaa27ed083e35412e2051ab64b06603593
Andrew Smith <smith.andrew.l@gmail.com>
2014-01-23 18:04:15 -0600

    Corrected Sum of Primes

--- CodeEval/src/com/smithandrewl/codeeval/sumofprimes/Main.scala
+++ CodeEval/src/com/smithandrewl/codeeval/sumofprimes/Main.scala
@@ -1,18 +1,14 @@
 package com.smithandrewl.codeeval.sumofprimes
 
-import scala.util.Random
-
 object Main extends App {
-  val rnd = new Random()
   def primes(start: Long): Stream[Long] = start match {
     case num if isPrime(num) => num #:: primes(num + 1)
     case num                 => primes(num + 1)
   }
 
   def isPrime(num: Long): Boolean = {
-    !(2 until Math.sqrt(num).toInt).exists(num % _ == 0)
+    !(2 to Math.sqrt(num).toInt).exists(num % _ == 0)
   }
 
   print(primes(2).take(1000).toList.sum)
-
 }
