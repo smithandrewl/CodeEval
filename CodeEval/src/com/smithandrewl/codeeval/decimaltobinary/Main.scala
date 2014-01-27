62e6ecc95bdadf1ffcf8ee81b2bd5a7197f259e0
Andrew Smith <smith.andrew.l@gmail.com>
2014-01-26 19:03:36 -0600

    Finished CodeEval DecimalToBinary problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/decimaltobinary/Main.scala
@@ -0,0 +1,25 @@
+package com.smithandrewl.codeeval.decimaltobinary
+
+import scala.annotation.tailrec
+
+object Main extends App {
+  val source = scala.io.Source.fromFile(args(0))
+  val lines  = source.getLines().filter(_.length > 0)
+
+   def decimalToBinary(num: Int): String = {
+     @tailrec
+     def iter(num: Int, acc: List[Int]): List[Int] = {
+       if (num == 0) acc
+       else iter(num / 2, num % 2 :: acc)
+     }
+
+     num match {
+       case 0   => "0"
+       case num => iter(num, List()).mkString("")
+     }
+   }
+
+  val res = lines.map(_.toInt).map(decimalToBinary)
+
+  print(res.mkString("\n"))
+}
