e6119b4df806372e82841101a75665e495ef5179
Andrew Smith <smith.andrew.l@gmail.com>
2014-02-01 21:45:08 -0600

    started work on codeeval rightmost char problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/lowestcommonancestor/Main.scala
@@ -0,0 +1,12 @@
+package com.smithandrewl.codeeval.lowestcommonancestor
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrew
+ * Date: 1/30/14
+ * Time: 7:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Main {
+
+}
