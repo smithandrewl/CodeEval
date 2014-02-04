3a8fd46f26eaa8811716c0156ed12c9b7fb2afe8
Andrew Smith <smith.andrew.l@gmail.com>
2014-02-03 20:57:19 -0600

    Added Code Eval "Remove Characters" problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/removecharacters/Main.scala
@@ -0,0 +1,20 @@
+package com.smithandrewl.codeeval.removecharacters
+
+object Main extends App {
+  def removeCharacters(str: String, chrs: String): String = {
+    val split = chrs.toCharArray;
+
+    str.filter(chr => !split.contains(chr))
+  }
+
+  def processLine(line: String): String = {
+    val parts = line.split(", ")
+
+    removeCharacters(parts(0), parts(1))
+  }
+
+  val source  = scala.io.Source.fromFile(args(0))
+  val lines   = source.getLines().filter(_.length > 0)
+
+  print(lines.map(processLine).mkString("\n"))
+}
