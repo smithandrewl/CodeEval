f671b1075617c61a2067faa3a96ebe5760c470fa
Andrew Smith <smith.andrew.l@gmail.com>
2014-02-06 14:25:11 -0600

    Added Code Eval   "Array Absurdity" problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/arrayabsurdity/Main.scala
@@ -0,0 +1,30 @@
+package com.smithandrewl.codeeval.arrayabsurdity
+
+import scala.annotation.tailrec
+
+object Main extends App {
+
+  def arrayAbsurdity(numbers: Array[Int]): Int = {
+    @tailrec
+    def iter(numbers: Array[Int], acc: Set[Int]): Int = numbers match {
+      case Array(head, _*) => acc.contains(head) match {
+        case true => head
+        case _ => iter(numbers.tail, acc + head)
+      }
+    }
+
+    iter(numbers, Set())
+  }
+
+  def processLine(line: String): String = {
+    val numbers = line.split(";")(1).split(",").map(_.toInt)
+
+    arrayAbsurdity(numbers).toString
+  }
+
+  val source = scala.io.Source.fromFile(args(0))
+  val lines  = source.getLines().filter(_.length > 0)
+
+  print(lines.map(processLine).mkString("\n"))
+
+}
