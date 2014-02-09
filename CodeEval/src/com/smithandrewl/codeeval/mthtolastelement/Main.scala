b53939638012fefdc89c9904a9421e11f4b5225e
Andrew Smith <smith.andrew.l@gmail.com>
2014-02-08 20:54:36 -0600

    Added Code Eval   "Mth to Last Element" problem

---
+++ CodeEval/src/com/smithandrewl/codeeval/mthtolastelement/Main.scala
@@ -0,0 +1,22 @@
+package com.smithandrewl.codeeval.mthtolastelement
+
+object Main extends App {
+
+  val source = scala.io.Source.fromFile(args(0))
+  val lines  = source.getLines().filter(_.length > 0)
+
+  def processLine(line: String): String = {
+    val numbers = line.split(" ")
+    val m       = numbers.last.toInt
+    val lst     = numbers.slice(0, numbers.length - 1)
+
+    mthToLast(lst, m)
+  }
+
+  def mthToLast(numbers: Array[String], m: Int): String = {
+    if(m > numbers.length) ""
+    else numbers(numbers.length - m)
+  }
+
+  print(lines.map(processLine).mkString("\n"))
+}
