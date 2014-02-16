d09fd936a1f84782db88d62c8758fae12f00f0f4
Andrew Smith <smith.andrew.l@gmail.com>
2014-02-16 15:28:09 -0600

    Improved the performance of Odd Numbers

--- Scala/src/com/smithandrewl/codeeval/scala/oddnumbers/Main.scala
+++ Scala/src/com/smithandrewl/codeeval/scala/oddnumbers/Main.scala
@@ -10,7 +10,6 @@ package com.smithandrewl.codeeval.scala.oddnumbers
     There is no input for this program.
  */
 object Main extends App {
-  val result = (1 to 99).filter(num => num % 2 != 0)
-
+  val result = 1.to(99, 2)
   print(result.mkString("\n"))
 }
