1f4981baa312ea9d6e5ed6c4c0014778e8c1b827
Andrew Smith <smith.andrew.l@gmail.com>
2014-03-03 19:22:39 -0600

    Made the Scala implementation of the "Decimal to Binary" problem faster

--- Scala/src/com/smithandrewl/codeeval/scala/decimaltobinary/Main.scala
+++ Scala/src/com/smithandrewl/codeeval/scala/decimaltobinary/Main.scala
@@ -6,20 +6,29 @@ object Main extends App {
   val source = scala.io.Source.fromFile(args(0))
   val lines  = source.getLines().filter(_.length > 0)
 
+  val sb = new StringBuilder()
+
    def decimalToBinary(num: Int): String = {
+
      @tailrec
-     def iter(num: Int, acc: List[Int]): List[Int] = {
-       if (num == 0) acc
-       else iter(num / 2, num % 2 :: acc)
+     def iter(num: Int): String = {
+       if (num == 0) sb.reverse.toString()
+       else {
+         sb.append(num % 2)
+         iter(num / 2)
+       }
      }
 
      num match {
        case 0   => "0"
-       case _ => iter(num, List()).mkString("")
+       case _ => {
+         sb.clear()
+         iter(num)
+       }
      }
    }
 
   val res = lines.map(_.toInt).map(decimalToBinary)
 
-  print(res.mkString("\n"))
+  res.foreach(println)
 }
