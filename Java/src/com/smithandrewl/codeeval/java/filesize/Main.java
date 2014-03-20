f0f157f37a4b3de5a4ea21cea4208249e90adddf
Andrew Smith <smith.andrew.l@gmail.com>
2014-03-19 22:04:59 -0500

    Added a Java implementation of the "File Size" problem

---
+++ Java/src/com/smithandrewl/codeeval/java/filesize/Main.java
@@ -0,0 +1,11 @@
+package com.smithandrewl.codeeval.java.filesize;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File   file = new File(args[0]);
+
+        System.out.println(file.length());
+    }
+}
